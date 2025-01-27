761a084c063f38776b338350abd19753154c7758
Therealkosbruh <realkoslulz@gmail.com>
2025-01-27 16:53:13 +0300

    added auth checker [1]

--- react/src/UserContext.jsx
+++ react/src/UserContext.jsx
@@ -1,24 +1,4 @@
-// import { createContext, useContext, useState } from 'react';
 
-// // Создаем контекст пользователя
-// const UserContext = createContext(null);
-
-// // Провайдер контекста
-// export const UserProvider = ({ children }) => {
-//   const [user, setUser] = useState(null);
-
-//   const login = (userData) => setUser(userData);
-//   const logout = () => setUser(null);
-
-//   return (
-//     <UserContext.Provider value={{ user, login, logout }}>
-//       {children}
-//     </UserContext.Provider>
-//   );
-// };
-
-// // Кастомный хук для использования контекста
-// export const useUser = () => useContext(UserContext);
 
 
 import { createContext, useContext, useState, useEffect } from 'react';
@@ -29,7 +9,7 @@ export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    // Восстанавливаем данные пользователя из localStorage
+  
     const storedUser = localStorage.getItem('user');
     if (storedUser) {
       setUser(JSON.parse(storedUser));
